32a167c25d509d60c2540957327fd4ff92aa3002
Artem Lipchik <tommerg09@gmail.com>
2019-03-12 17:08:10 +0300

    I implement function check(str, bracketsConfig), that for given brackets sequence will return true if it is correct and false otherwise

--- src/index.js
+++ src/index.js
@@ -1,3 +1,111 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  function lar(i, arr) {
+
+  outer: for (i; i < arr.length; i) {
+    if (i == -1) {i += 1};
+    if (arr[i] == '(') {
+      if (arr[i+1] == ')') {
+        delete arr[i];
+        delete arr[i+1];
+        i -= 1;
+        arr = arr.filter(element => element !== undefined);
+      }
+      else {i += 1} continue;
+    }
+    
+    if (arr[i] == '[') {
+      if (arr[i+1] == ']') {
+        delete arr[i];
+        delete arr[i+1];
+        i -= 1;
+        arr = arr.filter(element => element !== undefined);
+      }
+      else {i += 1} continue;
+    }
+
+    if (arr[i] == '{') {
+      if (arr[i+1] == '}') {
+        delete arr[i];
+        delete arr[i+1];
+        i -= 1;
+        arr = arr.filter(element => element !== undefined);
+      }
+      else {i += 1} continue;
+    }
+
+    if (arr[i] == '|') {
+      if (arr[i+1] == '|') {
+        delete arr[i];
+        delete arr[i+1];
+        i -= 1;
+        arr = arr.filter(element => element !== undefined);
+      }
+      else {i += 1} continue;
+    }
+
+    if (arr[i] == '1') {
+      if (arr[i+1] == '2') {
+        delete arr[i];
+        delete arr[i+1];
+        i -= 1;
+        arr = arr.filter(element => element !== undefined);
+      }
+      else {i += 1} continue;
+    }
+
+    if (arr[i] == '3') {
+      if (arr[i+1] == '4') {
+        delete arr[i];
+        delete arr[i+1];
+        i -= 1;
+        arr = arr.filter(element => element !== undefined);
+      }
+      else {i += 1} continue;
+    }
+
+    if (arr[i] == '5') {
+      if (arr[i+1] == '6') {
+        delete arr[i];
+        delete arr[i+1];
+        i -= 1;
+        arr = arr.filter(element => element !== undefined);
+      }
+      else {i += 1} continue;
+    }
+
+    if (arr[i] == '7') {
+      if (arr[i+1] == '7') {
+        delete arr[i];
+        delete arr[i+1];
+        i -= 1;
+        arr = arr.filter(element => element !== undefined);
+      }
+      else {i += 1} continue;
+    }
+
+    if (arr[i] == '8') {
+      if (arr[i+1] == '8') {
+        delete arr[i];
+        delete arr[i+1];
+        i -= 1;
+        arr = arr.filter(element => element !== undefined);
+      }
+      else {i += 1} continue;
+    }
+
+    else break outer;
+  }
+    if (arr == 0) {return true}
+    else {return false}
+
+  } 
+
+  var arr = str.split('');
+
+    var i = 0;
+    
+  return lar(i,arr);
+
+
 }
